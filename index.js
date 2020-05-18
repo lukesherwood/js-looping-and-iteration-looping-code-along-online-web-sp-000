c691df752825258c119cf9b6778461514478fc16
Luke Sherwood <luke.sherwood55@gmail.com>
2020-05-18 21:10:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+  for (i = 0, names.length > i, i++) {
+    
+  }
+}
+
+
+
+
 function countDown(n) {
   for (i = n; i > 0; i - 1 ) {
-    console.log(i)
+    console.log(n)
   }
 }
