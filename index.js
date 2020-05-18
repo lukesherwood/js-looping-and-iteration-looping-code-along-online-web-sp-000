1c92e18f544d26874556d1450bd8a9ad606860a2
Luke Sherwood <luke.sherwood55@gmail.com>
2020-05-18 21:12:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 // Code your solutions in this file
 function writeCards(names, event) {
-  for (i = 0, names.length > i, i++) {
-    
+  for (i = 0; names.length > i; i++) {
+    `Thank you, ${name[i]}, for the wonderful ${event} gift!`
   }
 }
-
-
 
 
 function countDown(n) {
